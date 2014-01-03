834001ad6f8c523b84303241f9e26d3a87704293
cjsund <cjsund@126.com>
2014-01-03 17:28:56 +0800

    update ranpasswd

--- ranpasswd.py
+++ ranpasswd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#coding:utf-8
-#filename=ranpasswd
+# coding:utf-8
+# filename=ranpasswd
 
 from random import sample
 
@@ -10,23 +10,12 @@ import string
 #
 
 class create():
-	"""
-	Generate a random password
 
-	#!/usr/bin/evn python
-	
-	import ranpasswd
+    def lens(self, length):
+        self.length = length
+        dic = string.digits + string.letters
+        return "".join(sample(dic, self.length))
 
-	password = ranpasswd.create()
-	password.len(10) #len(10) password length is 10
-
-
-	"""
-	def lens(self, length):
-		self.length = length
-		dic = string.digits + string.letters
-		return "".join(sample(dic, self.length))
-		
 
 if __name__ == "__main__":
-	print create.__doc__
+    print create.__doc__
